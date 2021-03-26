8e1a113116998611e9d51681b085ef30106d0abc
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-25 23:18:05 -0300

    Entrega Heron

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            double p, sp, area;
+            
+            Console.WriteLine("Digite os lados do Triângulo desejado.");
+            Console.Write("Lado 1..►");
+            a = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Lado 2..►");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lado 3..►");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("---------------------");
+
+            p = a + b + c;
+            sp = p / 2;
+            Console.WriteLine($"\nSemiperímetro..►► {sp} ");
+
+            area = Math.Sqrt(sp*(sp - a)*(sp - b)*(sp - c));
+            Console.WriteLine($"\nÁrea......►►► {area}");
+
+
+        }
+    }
+}
